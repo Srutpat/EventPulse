9ed8aa543d2f696a35d353029b47db492a54cf03
Shraddha Utpat <shraddha.utpat23@pccoepune.org>
2026-04-20 20:40:21 +0530

    Update SDWApprovals.jsx

--- frontend/campus-events-frontend/src/pages/sdw/SDWApprovals.jsx
+++ frontend/campus-events-frontend/src/pages/sdw/SDWApprovals.jsx
@@ -4,7 +4,7 @@ import DashboardLayout   from "../../layouts/DashboardLayout";
 import PageHeader        from "../../components/PageHeader";
 import ApprovalModal     from "../../components/ApprovalModal";
 import EventDetailDrawer from "../../components/EventDetailDrawer";
-import WorkflowBadge     from "../../components/WorkflowBadge";
+import WorkflowBadge     from "../../components/WorkFlowBadge";
 import { safeArray, getStatus, formatDateTime, filterForSDW } from "../../utils";
 import { Eye, CheckCircle, Info, IndianRupee } from "lucide-react";
 
